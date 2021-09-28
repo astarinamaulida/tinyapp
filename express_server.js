739650aebdb1f9e3943360d3324e73cf7dd963b7
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-28 13:26:19 -0400

    Fix error in redirecting to web

--- express_server.js
+++ express_server.js
@@ -3,51 +3,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-  const result = '';
+  let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for ( var i = 0; i < 7; i++ ) {
-    result + characters.charAt(Math.floor(Math.random() * 6));
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-// LISTEN
+const urlDatabase = {};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// GET
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
+// START
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -58,18 +32,31 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  const randomShort = generateRandomString();
+  urlDatabase[randomShort] = {
+    shortURL: randomShort,
+    longURL: req.body.longURL
+  }
+  res.redirect(`/urls/${randomShort}`);
+});
+
+//server generates a shortURL adds it to urlDatabase
 app.get("/urls/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
   let templateVars =
   {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL].longURL
   };
   res.render("urls_show", templateVars);
+  }
 });
 
-// POST
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+//redirect to longURL
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
+    const fullURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(fullURL);
+  }
 });
