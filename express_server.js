6e3b562f4fa4e711220649f173c47346609cacf9
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-28 05:35:04 -0400

    Add /urls/new path

--- express_server.js
+++ express_server.js
@@ -1,3 +1,6 @@
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -43,6 +46,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars =
   {
@@ -51,4 +58,3 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-
